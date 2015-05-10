18d595e9e12937b0c0ed5615c342102cf31b993f
Vivian Guillen <vivian@viviangb.com>
2015-05-10 15:39:45 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# bootstrap-theme
-A PyLadies bootstrap theme for non-documentation sites
+# Pyladies theme
+A PyLadies theme for non-documentation sites
+
+## Technologies: 
+
+- [Uikit](http://getuikit.com/index.html)
+- [Sass](http://sass-lang.com/)
+- [Gulp](http://gulpjs.com/)
+
+## To start edit: 
+
+- Open your terminal
+- Run `npm install`, this will install all the necesarry Gulp plugins 
+- Run `gulp watch`, this will make Gulp compile your `.sass` files
+
+
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    del = require('del');
+    plumber = require('gulp-plumber')
+
+
+// Styles
+gulp.task('styles', function() {
+  return sass('_assets/css/_sass/*.sass', { style: 'expanded' })
+  	.pipe(plumber())
+  	.pipe(autoprefixer({browsers: ['last 2 version']}))
+    .pipe(gulp.dest('_assets/css/'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+ 
+// Clean
+gulp.task('clean', function(cb) {
+    del(['_assets/css'], cb)
+});
+ 
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles');
+});
+ 
+// Watch
+gulp.task('watch', function() {
+ 
+  // Watch .sass files
+  gulp.watch('_assets/css/_sass/*.sass', ['styles']);
+ 
+});
